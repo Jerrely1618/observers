fe661c1f9c4bdd04825b4aee129bcfe2ae938153
Jerrely Vizueta <94874872+Jerrely1618@users.noreply.github.com>
2024-04-26 23:47:39 -0400

    deployment vercel

--- observers/src/app/pages/story/story.service.ts
+++ observers/src/app/pages/story/story.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,7 +11,8 @@ export class StoryService {
   constructor(private http: HttpClient) {}
 
   getStories(): Observable<any> {
-    return this.http.get(this.apiUrl);
+    return of([{ id: 1, title: 'Sample Story', content: 'This is a sample story.' }]);
+    // return this.http.get(this.apiUrl);
   }
 
   addStory(story: any): Observable<any> {
