f4ff4010909a9ae0fdb570117f1d0c19353e49a7
Jerrely Vizueta <94874872+Jerrely1618@users.noreply.github.com>
2024-04-26 22:36:47 -0400

    added daisy, formatter navbar

--- observers/tailwind.config.js
+++ observers/tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
 module.exports = {
   content:  ['./src/**/*.{html,ts}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('daisyui'), plugin(function({ addUtilities }) {
+      const newUtilities = {
+        '.double-border': {
+          borderTop: '2px double',
+          borderBottom: '2px double',
+        },
+      };
+      addUtilities(newUtilities);
+    }),],
 }
 
